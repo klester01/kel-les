1dc8df2c4b42fdbe8bc5a90695baef79e0360298
klester01 <klester01@aol.com>
2020-01-04 11:16:44 -0500

    changed appjs

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.textContent = 'Mozilla is cool!, ' + storedName;
 }
 
 }
